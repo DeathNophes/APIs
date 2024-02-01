6137683dd944f31eb866d8ae8c6e69f12f4f44f0
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-02-01 11:31:24 +0200

    Regular update

--- FinanceAPI/forex_api.py
+++ FinanceAPI/forex_api.py
@@ -1,4 +1,6 @@
 import requests
+from tkinter import *
+
 key = ''
 
 """
@@ -6,24 +8,47 @@ You need to create a registration at https://twelvedata.com/ to get a unique API
 """
 
 
-def get_forex_pair(symbol):
-    url = f"https://api.twelvedata.com/exchange_rate?symbol={symbol}&apikey={key}"
+def get_forex_pair():
+    pair = asset_entry.get()
+    display.delete(0, 'end')
+    url = f"https://api.twelvedata.com/exchange_rate?symbol={pair}&apikey={key}"
+
     try:
         request = requests.get(url)
         data = request.json()
 
         if 'status' in data.keys():
-            print('Error!')
-            exit()
-
-        print(f"{data['symbol']} -> {data['rate']}")
+            display.insert(0, 'Error!')
+        else:
+            display.insert(0, f"{data['symbol']} -> {data['rate']}")
 
     except requests.exceptions.RequestException as e:
         print('Error'), e
 
 
-pair = input()
-get_forex_pair(pair)
+"""
+Our Window
+"""
+
+main_window = Tk()
+main_window.title("Crypto price API")
+main_window.geometry("500x300")
+main_window.config(bg="#85929E")
+
+
+"""
+Our Entries and Buttons
+"""
+
+asset_entry = Entry(main_window, width=40)
+get_button = Button(main_window, text='Get', bg='yellow', width=20, command=get_forex_pair)
+display = Entry(main_window, width=50)
+
+asset_entry.pack(pady=10)
+get_button.pack(pady=5)
+display.pack(pady=50)
+
+main_window.mainloop()
 
 """
 Example output:
