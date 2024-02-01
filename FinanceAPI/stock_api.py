65f6b281421611bd1d1ac2250b62c870077a7c6c
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-02-01 11:31:29 +0200

    Regular update

--- FinanceAPI/stock_api.py
+++ FinanceAPI/stock_api.py
@@ -1,4 +1,6 @@
 import requests
+from tkinter import *
+
 key = ''
 
 """
@@ -6,31 +8,50 @@ You need to create a registration at https://twelvedata.com/ to get a unique API
 """
 
 
-def get_stock(symbol):
-    url = f"https://api.twelvedata.com/time_series?symbol={symbol}&interval=1min&apikey={key}"
-    data = ''
+def get_stock():
+    stock_name = asset_entry.get()
+    display.delete(0, 'end')
+    url = f"https://api.twelvedata.com/time_series?symbol={stock_name}&interval=1min&apikey={key}"
+
     try:
         request = requests.get(url)
         data = request.json()
 
         if data['status'] == 'error':
-            print('Error! Enter a valid stock!')
-            exit()
-
-        print(f"Stock: {data['meta']['symbol']}\n"
-              f"-Current price (USD): {data['values'][0]['close']}\n"
-              f"-Last refreshed: {data['values'][0]['datetime']}")
+            display.insert(0, 'Error! Enter a valid stock!')
+        else:
+            display.insert(0, f"{data['meta']['symbol']} "
+                              f"- Current price (USD): {data['values'][0]['close']}")
 
     except requests.exceptions.RequestException as e:
-        print('Error'), e
+        display.insert(0, 'Error'), e
 
 
-stock_name = input()
-get_stock(stock_name)
+"""
+Our Window
+"""
+
+main_window = Tk()
+main_window.title("Crypto price API")
+main_window.geometry("500x300")
+main_window.config(bg="#85929E")
+
+
+"""
+Our Entries and Buttons
+"""
+
+asset_entry = Entry(main_window, width=40)
+get_button = Button(main_window, text='Get', bg='yellow', width=20, command=get_stock)
+display = Entry(main_window, width=50)
+
+asset_entry.pack(pady=10)
+get_button.pack(pady=5)
+display.pack(pady=50)
+
+main_window.mainloop()
 
 """
 Example output:
-Stock: GOOGL
--Current price (USD): 131.77499
--Last refreshed: 2023-11-30 12:51:00
+AAPL - Current price (USD): 184.36000
 """
